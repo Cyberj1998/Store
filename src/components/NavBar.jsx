fdfb9f24cfada15d200f5fd65a347ed978c1021e
jose <cyberjay826@gmail.com>
2026-03-06 08:16:31 -0500

    final touches

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom"
 import Logo from '../assets/images/logo.png'
 import CartIcon from '../assets/images/cart.png'
 import useCartStore from "../store/CartSlice"
-import { useEffect, useState } from "react"
+import Shop from '../assets/images/shop.png'
+import ShopIcon from '../assets/images/shopIcon.png'
 
 const NavBar = () => {
 
@@ -10,14 +11,19 @@ const NavBar = () => {
 
   return ( 
     <nav className="fixed top-0 left-0 w-full bg-gray-200 h-14 flex items-center justify-between px-6 z-50">
-      <Link to="/" className="text-lg font-bold text-gray-800 hover:text-blue-600 transition-colors">
-        Shop
+      <Link to="/" className="text-lg font-bold text-gray-800 hover:text-blue-600 transition-colors flex justify-center items-center">
+        <img 
+          src={Shop}
+          alt="shop icon" 
+          className="h-10 w-10"
+        />
+        <p>Shop</p>
       </Link>
 
       <img 
-        src={Logo} 
+        src={ShopIcon} 
         alt="Logo" 
-        className="h-12 w-12 object-contain"
+        className="h-12 w-12 object-contain bg-gray-400 rounded-full"
       />
 
       <Link 
