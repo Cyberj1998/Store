a725f50736040c792a53dddc24e4d216a02d4a42
jose <cyberjay826@gmail.com>
2026-04-13 10:10:49 -0400

    small changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({ product }) => {
   const addToCart = useCartStore(state=>state.addToCart) 
 
   return ( 
-    <div className="w-70 md:w-60 max-md:w-50 max-sm:w-40 h-auto max-md:h-auto bg-white p-4 flex flex-col items-center gap-4 rounded-3xl m-3 border border-gray-300 shadow-md hover:shadow-xl transition-shadow duration-300">
+    <div className="w-70 md:w-60 max-md:w-70 max-sm:w-45 h-auto max-md:h-auto bg-white p-4 flex flex-col items-center gap-4 rounded-3xl m-3 border border-gray-300 shadow-md hover:shadow-xl transition-shadow duration-300">
       <img
         src={product.image}
         alt={product.name}
