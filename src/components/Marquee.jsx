33f5ea8f60914ece8fa8aba2cab2b8c7d06254cd
jose <cyberjay826@gmail.com>
2026-08-10 09:42:03 -0400

    small typo fix in tailwind

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,13 +5,13 @@ const Marquee = () => {
   const phrases = ['test phrase 1', 'test phrase 2', 'test phrase 3']
 
   const MarqueeContent = () => (
-    <div className="flex-shrink-0 flex flex-row items-center bg-[#4f83dd] whitespace-nowrap">
+    <div className="shrink-0 flex flex-row items-center bg-[#4f83dd] whitespace-nowrap">
       {phrases.map((phrase, i) => (
         <React.Fragment key={i}>
           <h3 className="text-[50px] p-0 m-0 font-extrabold leading-none uppercase text-[#eed7af] [text-box: trim-both_cap_alphabetic]">
             {phrase}
           </h3>
-          <img src={Logo} alt="logo" className="h-15 w-20 m-2 flex-shrink-0" />
+          <img src={Logo} alt="logo" className="h-15 w-20 m-2 shrink-0" />
         </React.Fragment>
       ))}
     </div>
